88f988b40ad9b61f67d3c7afc2600cad384afea6
Warm Beer <mickvd99@gmail.com>
2022-09-29 19:22:15 +0200

    feat: added backgrounds

--- types/Backgrounds.ts
+++ types/Backgrounds.ts
@@ -12,6 +12,10 @@ export const Backgrounds: Array<Background> = [
     new Background("Transparent", "/images/others/Transparent.png"),
     new Background("Black", "/images/others/Black.png"),
     new Background("Rainbow Foustling", "/images/nfts/RainbowFoustling.png"),
+    new Background("Fishy Foustling", "/images/nfts/FishyFoustling.png"),
     new Background("The Eyes", "/images/nfts/TheEyes.png"),
     new Background("Julia Jewels", "/images/nfts/JuliaJewels.png"),
+    new Background("Cheeky Beak", "/images/nfts/CheekyBeak.png"),
+    new Background("Mouse au Chocolate", "/images/nfts/MouseAuChocolate.png"),
+    new Background("Classic Animation", "/images/nfts/ClassicAnimation.png"),
 ];
